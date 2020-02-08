edea7d6ec55dbcd2a6239970798ec50bfe0e5b36
mamanipatricia <patttymq@gmail.com>
2020-02-08 10:44:17 -0400

    react iteratos, states, props, variables, objects, jsx, importing components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+// importing other components
+
+// main
+const App = ({ tgt }) => {
+  // variables (states=estados) <- useState 
+  // variables del componente, no confundir con props
+  const [name, setName] = useState("PATRICIA")
+  const [lastName, setLastName] = useState("MAMANI")
+  const [listArticles, setListArticles] = useState([{ id: 2, name: "two" }, { id: 3, name: "three" }, { id: 5, name: "five" }])
+
+  // methods and functions
+  const updateName = (name, lastName) => {
+    console.log(lastName)
+    setName(name)
+    setLastName(lastName)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HOLA {name} {lastName}</h1>
+      <button onClick={() => updateName("OSCAR", "CHOQUE")}>Click me</button>
+      <ol>
+        {
+          listArticles.map((article) => {
+            return (
+              <li>ID: {article.id} NAME : {article.name}
+              </li>)
+          })
+        }
+      </ol>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App rrr={{city: "la paz"}} array={[1,2,3]} tgt={2+3} text="jose" TEXT2="Hose3"/>,
+    document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
